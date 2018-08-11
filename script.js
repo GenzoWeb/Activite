a5fcd9ff668d8510d5482269a713e9616c618c93
Genzo <genzo10@hotmail.fr>
2018-08-11 15:00:35 +0200

    Ecriture du fichier script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+var newTexte = document.createElement('p');
+
+var texte = document.createTextNode("Voici le texte que j'ai réalisé avec mon fichier javascript !!");
+
+newTexte.appendChild(texte);
+
+document.body.appendChild(newTexte);
